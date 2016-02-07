27655a4308860ea420e5066339ea1bf6a9e4b1f0
Mariana Naidenova <mariananaidenova@mail.bg>
2016-02-07 20:41:47 +0200

    Add Repositories and ITasteItDbContext.cs

---
+++ TasteIt/Data/TasteIt.Data/ITasteItDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace TasteIt.Data
+{
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    using TasteIt.Data.Models;
+
+    public interface ITasteItDbContext
+    {
+        int SaveChanges();
+
+        IDbSet<User> Users { get; set; }
+
+        //TODO: Add models
+     
+        DbSet<TEntry> Set<TEntry>() where TEntry : class;
+
+        DbEntityEntry<TEntry> Entry<TEntry>(TEntry entity) where TEntry : class;
+    }
+}
--- TasteIt/Data/TasteIt.Data/TasteItDbContext.cs
+++ TasteIt/Data/TasteIt.Data/TasteItDbContext.cs
@@ -3,13 +3,14 @@
     using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
 
-
-    public class TasteItDbContext : IdentityDbContext<User>
+    public class TasteItDbContext : IdentityDbContext<User>, ITasteItDbContext
     {
         public TasteItDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+
+        //TODO: Add models
 
         public static TasteItDbContext Create()
         {
