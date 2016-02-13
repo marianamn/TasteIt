3908f5361c3f6ba97a1208ca91bc987dadfc9d71
Mariana Naidenova <mariananaidenova@mail.bg>
2016-02-13 16:30:48 +0200

    separate Data and Data.Common

--- TasteIt/Data/TasteIt.Data/TasteItDbContext.cs
+++ TasteIt/Data/TasteIt.Data/TasteItDbContext.cs
@@ -3,8 +3,11 @@
     using System.Data.Entity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
+    using System.Linq;
+    using Common.Models;
+    using System;
 
-    public class TasteItDbContext : IdentityDbContext<User>, ITasteItDbContext
+    public class TasteItDbContext : IdentityDbContext<User>
     {
         public TasteItDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -26,6 +29,33 @@
         public static TasteItDbContext Create()
         {
             return new TasteItDbContext();
+        }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditInfo && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditInfo)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.Now;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
         }
     }
 }
